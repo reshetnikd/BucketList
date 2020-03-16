4ed7535b97d98791fdc15b43f65a364277c959d6
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-16 20:44:17 +0200

    Try out some code

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,14 +8,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
+struct ContentView: View {
+    let values = [1, 5, 3, 6, 2, 9].sorted()
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted()
+    var loadingState = LoadingState.loading
+    
+    var body: some View {
+        Group {
+            if loadingState == .loading {
+                LoadingView()
+            } else if loadingState == .success {
+                SuccessView()
+            } else if loadingState == .failed {
+                FailedView()
+            }
+        }
+//        List(users) { user in
+//            Text("\(user.lastName), \(user.firstName)")
+//                .onTapGesture {
+//                    let str = "Test Message"
+//                    let fileName = "message.txt"
+//                    FileManager.writeToDocumentsDirectory(data: str, file: fileName)
+//                }
+//        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension FileManager {
+    static func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = self.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+    
+    static func writeToDocumentsDirectory<T: StringProtocol>(data: T, file: String) {
+        let url = self.getDocumentsDirectory().appendingPathComponent(file)
+        
+        do {
+            try data.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
